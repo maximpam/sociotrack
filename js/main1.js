dc546cb058793fd4ab3f186ff7a270c3236ce633
Maxim <maximpam@ukr.net>
2019-02-15 23:36:16 +0200

    Описание коммита

---
+++ js/main1.js
@@ -0,0 +1,5 @@
+import {
+    social
+} from "./social.json";
+
+console.log(social[0].name);
---
+++ js/social.js
@@ -0,0 +1,15 @@
+export let social = [{
+        "name": "Facebook",
+        "followers": 1701,
+        "logo": "img\\Facebook-color.svg",
+        "login": "@abhisek.daas",
+        "change": "+4"
+    },
+    {
+        "name": "Twitter",
+        "followers": 1567,
+        "logo": "img\\Facebook-color.svg",
+        "login": "@abhisek.daas",
+        "change": "+4"
+    }
+];
